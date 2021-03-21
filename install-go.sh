2e6ca9ab7cba25b0b89ac1eba2db4f203b91dfb7
Rubi <14269809+codenoid@users.noreply.github.com>
2021-03-21 16:51:57 +0700

    Create install-go.sh

---
+++ install-go.sh
@@ -0,0 +1,27 @@
+# codenoid
+# https://gist.github.com/codenoid/4806365032bb4ed62f381d8a76ddb8e6
+printf "Checking latest Go version...\n";
+LATEST_GO_VERSION="$(curl --silent https://golang.org/VERSION?m=text)";
+LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz "
+
+printf "cd to home ($USER) directory \n"
+cd "/home/$USER"
+
+printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n";
+curl -OJ -L --progress-bar https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz
+
+printf "Extracting file...\n"
+tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
+
+export GOROOT="/home/$USER/go"
+export GOPATH="/home/$USER/go/packages"
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+
+printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR ~/.bashrc  ⚠️
+export GOROOT="/home/$USER/go"
+export GOPATH="/home/$USER/go/packages"
+export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+\n'
+
+printf "You are ready to Go!";
+go version
