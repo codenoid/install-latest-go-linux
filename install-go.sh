618f3b8c19d568089ea8b7570c1d9962f8d8a3b1
codenoid <14269809+codenoid@users.noreply.github.com>
2023-08-16 18:55:46 +0700

    Update install-go.sh

--- install-go.sh
+++ install-go.sh
@@ -1,27 +1,28 @@
 # codenoid
 # https://gist.github.com/codenoid/4806365032bb4ed62f381d8a76ddb8e6
 printf "Checking latest Go version...\n";
-LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text)";
-LATEST_GO_DOWNLOAD_URL="https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz "
+LATEST_GO_VERSION="$(curl --silent https://go.dev/VERSION?m=text | head -n 1)";
+LATEST_GO_DOWNLOAD_URL="https://go.dev/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz"
 
 printf "cd to home ($USER) directory \n"
-cd "/home/$USER"
+cd $HOME
 
 printf "Downloading ${LATEST_GO_DOWNLOAD_URL}\n\n";
-curl -OJ -L --progress-bar https://golang.org/dl/${LATEST_GO_VERSION}.linux-amd64.tar.gz
+curl -OJ -L --progress-bar $LATEST_GO_DOWNLOAD_URL
 
 printf "Extracting file...\n"
 tar -xf ${LATEST_GO_VERSION}.linux-amd64.tar.gz
 
-export GOROOT="/home/$USER/go"
-export GOPATH="/home/$USER/go/packages"
+export GOROOT="$HOME/go"
+export GOPATH="$HOME/go/packages"
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 
 printf '⚠️  ADD (OR REPLACE) THIS LINE BELOW TO YOUR ~/.bashrc  ⚠️
-export GOROOT="/home/$USER/go"
-export GOPATH="/home/$USER/go/packages"
+
+export GOROOT="$HOME/go"
+export GOPATH="$HOME/go/packages"
 export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 \n'
 
-printf "You are ready to Go!\n";
+printf "You are ready to Go!";
 go version
